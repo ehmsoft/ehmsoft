c46c2549e6307d3584a55f642b4c71517c6b5101
Harold <sancospi@gmail.com>
2011-05-20 20:55:41 -0500

    Primer commit

---
+++ src/ehmsoft/ScreenMain.java
@@ -0,0 +1,19 @@
+package ehmsoft;
+
+import net.rim.device.api.ui.container.MainScreen;
+
+/**
+ * A class extending the MainScreen class, which provides default standard
+ * behavior for BlackBerry GUI applications.
+ */
+public final class ScreenMain extends MainScreen
+{
+    /**
+     * Creates a new ScreenMain object
+     */
+    public ScreenMain()
+    {        
+        // Set the displayed title of the screen       
+        setTitle("MyTitle");
+    }
+}
