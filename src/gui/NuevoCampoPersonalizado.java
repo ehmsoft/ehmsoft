371b3597a7732db25994dcc697343680d800558c
Harold <sancospi@gmail.com>
2011-05-24 15:29:25 -0500

    Se creo la pantalla de nuevo campo personalizado con su controlador

---
+++ src/gui/NuevoCampoPersonalizado.java
@@ -0,0 +1,72 @@
+package gui;
+
+import net.rim.device.api.ui.MenuItem;
+import net.rim.device.api.ui.UiApplication;
+import net.rim.device.api.ui.component.BasicEditField;
+import net.rim.device.api.ui.component.CheckboxField;
+import net.rim.device.api.ui.component.Dialog;
+import net.rim.device.api.ui.container.MainScreen;
+import net.rim.device.api.ui.container.VerticalFieldManager;
+
+public class NuevoCampoPersonalizado extends MainScreen {
+
+	/**
+	 * Crea una nueva ventana para la creación de un nuevo campo personalizado
+	 */
+	private BasicEditField _txtNombre;
+	private CheckboxField _cbObligatorio;
+	private BasicEditField _txtLongMax;
+	private BasicEditField _txtLongMin;
+	public NuevoCampoPersonalizado() {
+		super(MainScreen.VERTICAL_SCROLL | MainScreen.VERTICAL_SCROLLBAR);
+		// TODO Auto-generated constructor stub
+		setTitle("Nuevo campo personalizado");
+		VerticalFieldManager vertical = new VerticalFieldManager();
+		
+		_txtNombre = new BasicEditField(BasicEditField.NO_NEWLINE);
+		_txtNombre.setLabel("Nombre: ");
+		
+		_cbObligatorio = new CheckboxField(" Obligatorio", false);
+		
+		_txtLongMax = new BasicEditField(BasicEditField.NO_NEWLINE | BasicEditField.FILTER_INTEGER);
+		_txtLongMax.setLabel("Longitud máxima: ");
+		
+		_txtLongMin = new BasicEditField(BasicEditField.NO_NEWLINE | BasicEditField.FILTER_INTEGER);
+		_txtLongMin.setLabel("Longitud mínima: ");
+		
+		vertical.add(_txtNombre);
+		vertical.add(_cbObligatorio);
+		vertical.add(_txtLongMax);
+		vertical.add(_txtLongMin);
+		
+		add(vertical);
+		addMenuItem(menuGuardar);
+	}
+	
+	private final MenuItem menuGuardar = new MenuItem("Guardar", 0, 0) {
+
+		public void run() {
+			// TODO Auto-generated method stub
+			if(Integer.parseInt(_txtLongMax.getText()) >= Integer.parseInt(_txtLongMin.getText()))
+				UiApplication.getUiApplication().popScreen(getScreen());
+			else
+				Dialog.alert("La longitud mínima no puede ser mayor que la longitud máxima, corrija y guarde nuevamente");
+		}
+	};
+	
+	public String getNombre(){
+		return _txtNombre.getText();
+	}
+	
+	public Boolean isObligatorio(){
+		return new Boolean(_cbObligatorio.getChecked());
+	}
+	
+	public int getLongMax(){
+		return Integer.parseInt(_txtLongMax.getText());
+	}
+	
+	public int getLongMin(){
+		return Integer.parseInt(_txtLongMin.getText());
+	}
+}
---
+++ src/gui/NuevoCampoPersonalizadoController.java
@@ -0,0 +1,33 @@
+package gui;
+
+import net.rim.device.api.ui.component.Dialog;
+import core.CampoPersonalizado;
+import persistence.Persistence;
+
+public class NuevoCampoPersonalizadoController {
+	private CampoPersonalizado _campo;
+	private NuevoCampoPersonalizado _screen;
+	
+	public NuevoCampoPersonalizadoController() {
+		_screen = new NuevoCampoPersonalizado();
+	}
+	
+	public NuevoCampoPersonalizado getScreen(){
+		return _screen;
+	}
+	
+	public CampoPersonalizado getCampo(){
+		return _campo;
+	}
+	
+	public void guardarCampo(){
+		Persistence guardado = new Persistence();
+		_campo = new CampoPersonalizado(_screen.getNombre(), null, _screen.isObligatorio(), _screen.getLongMax(), _screen.getLongMin());
+		try {
+			guardado.guardarCampoPersonalizado(_campo, null);
+		} catch (Exception e) {
+			Dialog.alert(e.toString());
+			e.printStackTrace();
+		}
+	}
+}
