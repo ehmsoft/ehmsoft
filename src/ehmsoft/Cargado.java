3f77ca5cd0c252afd927b11065bf001a9cf948dd
elfotografo007 <elfotografo007@gmail.com>
2011-05-24 12:21:42 -0500

    Se agregan throws de cargado
    Change-Id: Iba3118d1d1c0050e2caceb4fe2c8b1a770d23e6d

--- src/ehmsoft/Cargado.java
+++ src/ehmsoft/Cargado.java
@@ -6,15 +6,15 @@ import core.*;
 
 
 public interface Cargado {
-	public Vector consultarDemandantes();
-	public Vector consultarDemandados();
-	public Vector consultarPersonas();
-	public Persona consultarPersona(String id_persona);
-	public Vector consultarProcesos();
-	public Proceso consultarProceso(String id_proceso);
-	public Vector consultarActuaciones(Proceso proceso);
-	public Actuacion consultarActuacion(String id_actuacion);
-	public Vector consultarJuzgados();
-	public Juzgado consultarJuzgado(String id_juzgado);
+	public Vector consultarDemandantes() throws Exception;
+	public Vector consultarDemandados() throws Exception;
+	public Vector consultarPersonas() throws Exception;
+	public Persona consultarPersona(String id_persona) throws Exception;
+	public Vector consultarProcesos() throws Exception;
+	public Proceso consultarProceso(String id_proceso) throws Exception;
+	public Vector consultarActuaciones(Proceso proceso) throws Exception;
+	public Actuacion consultarActuacion(String id_actuacion) throws Exception;
+	public Vector consultarJuzgados() throws Exception;
+	public Juzgado consultarJuzgado(String id_juzgado) throws Exception;
 	
 }
