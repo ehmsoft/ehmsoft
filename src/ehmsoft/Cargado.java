7238cf9047485d924634411dfdd24a5d2611d617
elfotografo007 <elfotografo007@gmail.com>
2011-05-24 10:34:35 -0500

    Se modifican las interfaces de Cargado y Guardado para excepciones -Se agrega la cláusula throws -Se agrega el argumento id_proceso para guardar actuación y campo
    Change-Id: Id1567134863d4069032be79c0ca1feb8452e4023

--- src/ehmsoft/Cargado.java
+++ src/ehmsoft/Cargado.java
@@ -5,6 +5,7 @@ import java.util.Vector;
 import core.*;
 
 
+<<<<<<< OURS
 public interface Cargado {
 	public Vector consultarDemandantes();
 	public Vector consultarDemandados();
@@ -16,5 +17,16 @@ public interface Cargado {
 	public Actuacion consultarActuacion(String id_actuacion);
 	public Vector consultarJuzgados();
 	public Juzgado consultarJuzgado(String id_juzgado);
+=======
+public interface Cargado {
+	public Vector consultarPersonas() throws Exception;
+	public Persona consultarPersona(String id_persona) throws Exception;
+	public Vector consultarProcesos() throws Exception;
+	public Proceso consultarProceso(String id_proceso) throws Exception;
+	public Vector consultarActuaciones(Proceso proceso) throws Exception;
+	public Actuacion consultarActuacion(String id_actuacion) throws Exception;
+	public Vector consultarJuzgados() throws Exception;
+	public Juzgado consultarJuzgado(String id_juzgado) throws Exception;
+>>>>>>> THEIRS
 	
 }
--- src/ehmsoft/Guardado.java
+++ src/ehmsoft/Guardado.java
@@ -4,23 +4,23 @@ import core.*;
 
 public interface Guardado {
 	//Personas
-	public void actualizarPersona(Persona persona);
-	public void guardarPersona(Persona persona);
-	public void borrarPersona(Persona persona);
+	public void actualizarPersona(Persona persona) throws Exception;
+	public void guardarPersona(Persona persona) throws Exception;
+	public void borrarPersona(Persona persona) throws Exception;
 	//Juzgados
-	public void actualizarJuzgado(Juzgado juzgado);
-	public void guardarJuzgado(Juzgado juzgado);
-	public void borrarJuzgado(Juzgado juzgado);
+	public void actualizarJuzgado(Juzgado juzgado) throws Exception;
+	public void guardarJuzgado(Juzgado juzgado) throws Exception;
+	public void borrarJuzgado(Juzgado juzgado) throws Exception;
 	//Actuaciones
-	public void actualizarActuacion(Actuacion actuacion);
-	public void guardarActuacion(Actuacion actuacion);
-	public void borrarActuacion(Actuacion actuacion);
+	public void actualizarActuacion(Actuacion actuacion) throws Exception;
+	public void guardarActuacion(Actuacion actuacion, String id_proceso) throws Exception;
+	public void borrarActuacion(Actuacion actuacion) throws Exception;
 	//Campos Personalizados
-	public void actualizarCampoPersonalizado(CampoPersonalizado campo);
-	public void guardarCampoPersonalizado(CampoPersonalizado campo);
-	public void borrarCampoPersonalizado(CampoPersonalizado campo);
+	public void actualizarCampoPersonalizado(CampoPersonalizado campo) throws Exception;
+	public void guardarCampoPersonalizado(CampoPersonalizado campo, String id_proceso) throws Exception;
+	public void borrarCampoPersonalizado(CampoPersonalizado campo) throws Exception;
 	//Procesos
-	public void actualizarProceso(Proceso proceso);
-	public void guardarProceso(Proceso proceso);
-	public void borrarProceso(Proceso proceso);
+	public void actualizarProceso(Proceso proceso) throws Exception;
+	public void guardarProceso(Proceso proceso) throws Exception;
+	public void borrarProceso(Proceso proceso) throws Exception;
 }
