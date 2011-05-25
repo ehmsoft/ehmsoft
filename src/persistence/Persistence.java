abcc27cb35ba63b4eb2bdaa675e9c5ade145b177
Esteban Santa <estebans26@hotmail.com>
2011-05-25 02:56:15 -0500

    se implementa guardar y actualizar en demandantes, demandados, juzgados. guardar en actuaciones

--- src/persistence/Persistence.java
+++ src/persistence/Persistence.java
@@ -2,6 +2,10 @@ package persistence;
 
 import java.util.Vector;
 
+import net.rim.device.api.database.Database;
+import net.rim.device.api.database.DatabaseFactory;
+import net.rim.device.api.database.Statement;
+
 import core.*;
 import ehmsoft.Cargado;
 import ehmsoft.Guardado;
@@ -10,13 +14,70 @@ public class Persistence implements Cargado, Guardado {
 	private ConnectionManager connMgr;
 
 	public void actualizarPersona(Persona persona) throws Exception {
-		// TODO Auto-generated method stub
+		/**
+		 * @param tipo	1 para demandante, 2 para demandado
+		 **/
+		Database d = null;
+		d= DatabaseFactory.open(connMgr.getDbLocation());
+		if(persona.getTipo()==1){
+			Statement stPersona1 = d.createStatement("INSERT INTO demandantes VALUES(persona.getId_persona()," +
+					"persona.getId()," +
+					"persona.getNombre()," +
+					"persona.getTelefono()," +
+					"persona.getDireccion()," +
+					"persona.getCorreo()," +																		
+					"persona.getNombre())");
+			stPersona1.prepare();
+			stPersona1.execute();
+			stPersona1.close();
+		}
+		if(persona.getTipo()==2){
+			Statement stPersona2 = d.createStatement("INSERT INTO demandados VALUES(persona.getId_persona()," +
+					"persona.getId()," +
+					"persona.getNombre()," +
+					"persona.getTelefono()," +
+					"persona.getDireccion()," +
+					"persona.getCorreo()," +																		
+					"persona.getNombre())");
+			stPersona2.prepare();
+			stPersona2.execute();
+			stPersona2.close();
+		}
+		d.close();
 		
 	}
 
 	public void guardarPersona(Persona persona) throws Exception {
-		// TODO Auto-generated method stub
-		
+		 /**
+		 * @param tipo	1 para demandante, 2 para demandado
+		 **/
+		Database d = null;
+		d= DatabaseFactory.open(connMgr.getDbLocation());
+		if(persona.getTipo()==1){
+			Statement stPersona1 = d.createStatement("INSERT INTO demandantes VALUES(NULL, " +
+					"persona.getId()," +
+					"persona.getNombre()," +
+					"persona.getTelefono()," +
+					"persona.getDireccion()," +
+					"persona.getCorreo()," +																		
+					"persona.getNombre())");
+			stPersona1.prepare();
+			stPersona1.execute();
+			stPersona1.close();
+		}
+		if(persona.getTipo()==2){
+			Statement stPersona2 = d.createStatement("INSERT INTO demandados VALUES(NULL, " +
+					"persona.getId()," +
+					"persona.getNombre()," +
+					"persona.getTelefono()," +
+					"persona.getDireccion()," +
+					"persona.getCorreo()," +																		
+					"persona.getNombre())");
+			stPersona2.prepare();
+			stPersona2.execute();
+			stPersona2.close();
+		}
+		d.close();
 	}
 
 	public void borrarPersona(Persona persona) throws Exception {
@@ -25,13 +86,34 @@ public class Persistence implements Cargado, Guardado {
 	}
 
 	public void actualizarJuzgado(Juzgado juzgado) throws Exception {
-		// TODO Auto-generated method stub
+		Database d = null;
+		d= DatabaseFactory.open(connMgr.getDbLocation());
+			Statement stJuzgado = d.createStatement("INSERT INTO juzgados VALUES( juzgado.getId_juzgado(),"+
+					"juzgado.getNombre(),"+
+					"juzgado.getCiudad(),"+
+					"juzgado.getTelefono(),"+
+					"juzgado.getDireccion(),"+ 
+					"juzgado.getTipo())");
+			stJuzgado.prepare();
+			stJuzgado.execute();
+			stJuzgado.close();
+			d.close();
 		
 	}
 
 	public void guardarJuzgado(Juzgado juzgado) throws Exception {
-		// TODO Auto-generated method stub
-		
+		Database d = null;
+		d= DatabaseFactory.open(connMgr.getDbLocation());
+			Statement stJuzgado = d.createStatement("INSERT INTO juzgados VALUES( NULL,"+
+					"juzgado.getNombre(),"+
+					"juzgado.getCiudad(),"+
+					"juzgado.getTelefono(),"+
+					"juzgado.getDireccion(),"+
+					"juzgado.getTipo())");
+			stJuzgado.prepare();
+			stJuzgado.execute();
+			stJuzgado.close();
+			d.close();
 	}
 
 	public void borrarJuzgado(Juzgado juzgado) throws Exception {
@@ -44,9 +126,19 @@ public class Persistence implements Cargado, Guardado {
 		
 	}
 
-	public void guardarActuacion(Actuacion actuacion, String id_proceso)
-			throws Exception {
-		// TODO Auto-generated method stub
+	public void guardarActuacion(Actuacion actuacion, String id_proceso) throws Exception {
+		Database d = null;
+		d= DatabaseFactory.open(connMgr.getDbLocation());
+			Statement stActiacion = d.createStatement("INSERT INTO actuciones VALUES( NULL,"+
+					"id_proceso,"+
+					"actuacion.getJuzgado(),"+
+					"actuacion.getFecha(),"+
+					"actuacion.getFechaProxima(),"+
+					"actuacion.getDescripcion())");
+			stActiacion.prepare();
+			stActiacion.execute();
+			stActiacion.close();
+			d.close();
 		
 	}
 
@@ -61,9 +153,9 @@ public class Persistence implements Cargado, Guardado {
 		
 	}
 
-	public void guardarCampoPersonalizado(CampoPersonalizado campo,
-			String id_proceso) throws Exception {
+	public void guardarCampoPersonalizado(CampoPersonalizado campo,String id_proceso) throws Exception {
 		// TODO Auto-generated method stub
+		
 		
 	}
 
