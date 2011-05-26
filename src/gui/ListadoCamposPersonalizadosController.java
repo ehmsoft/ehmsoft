cba5407a3ad700444da68281eea6b791615c9116
Harold <sancospi@gmail.com>
2011-05-25 21:05:33 -0500

    Se creo la lista de campos personalizados, aun en desarrollo
    Change-Id: I7241ed155fc479e432b2d2758a0e51eb5469488a

---
+++ src/gui/ListadoCamposPersonalizadosController.java
@@ -0,0 +1,57 @@
+package gui;
+
+import java.util.Enumeration;
+import java.util.Vector;
+
+import net.rim.device.api.ui.UiApplication;
+import net.rim.device.api.ui.component.Dialog;
+import persistence.Persistence;
+import core.CampoPersonalizado;
+
+public class ListadoCamposPersonalizadosController {
+
+	private Persistence _persistencia;
+	private Vector _vectorCampos;
+	private ListadoCamposPersonalizados _screen;
+
+	public ListadoCamposPersonalizadosController() {
+		_persistencia = new Persistence();
+
+		_screen = new ListadoCamposPersonalizados();
+		addCampos();
+	}
+
+	public void setVectorCampos(Vector campos) {
+		_vectorCampos = campos;
+		addCampos();
+	}
+
+	private void addCampos() {
+		Enumeration index;
+		try {
+			index = _vectorCampos.elements();
+			while (index.hasMoreElements())
+				_screen.addCampo(index.nextElement());
+		} catch (NullPointerException e) {
+
+		} catch (Exception e) {
+			Dialog.alert(e.toString());
+		}
+	}
+
+	public CampoPersonalizado getSelected() {
+		NuevoCampoPersonalizadoController nuevoCampo = new NuevoCampoPersonalizadoController();
+		if (String.class.isInstance(_screen.getSelected())) {
+			UiApplication.getUiApplication().pushModalScreen(
+					nuevoCampo.getScreen());
+			nuevoCampo.guardarCampo();
+			_screen.addCampo(nuevoCampo.getCampo());
+			return nuevoCampo.getCampo();
+		} else
+			return (CampoPersonalizado) _screen.getSelected();
+	}
+
+	public ListadoCamposPersonalizados getScreen() {
+		return _screen;
+	}
+}
