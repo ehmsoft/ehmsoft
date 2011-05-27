8e39e4ee65ef615fea7f8b5d752946afc70ad4d6
Harold <sancospi@gmail.com>
2011-05-27 01:33:15 -0500

    Agregadas las clasesvacias  para el listado de actuaciones
    Change-Id: I21e13c8a304f6737adac98600a1459999e9ea3fd

---
+++ src/gui/ListadoActuaciones.java
@@ -0,0 +1,15 @@
+package gui;
+
+import net.rim.device.api.ui.container.MainScreen;
+
+public class ListadoActuaciones extends MainScreen {
+
+	/**
+	 * 
+	 */
+	public ListadoActuaciones() {
+		super(MainScreen.VERTICAL_SCROLL | MainScreen.VERTICAL_SCROLLBAR);
+		// TODO Auto-generated constructor stub
+	}
+
+}
