c84ae32f63065f9f1d4db0cbd87bdc6ae5c91eb9
sancospi <sancospi@gmail.com>
2011-06-14 19:29:24 -0500

    Se comenzo la implementacion de ver proceso
    Change-Id: I2d873db35583f521f9226576e3dfc751b98bf722

--- src/gui/VerProceso.java
+++ src/gui/VerProceso.java
@@ -1,7 +1,11 @@
 package gui;
 
 import net.rim.device.api.ui.component.BasicEditField;
+import net.rim.device.api.ui.component.DateField;
 import net.rim.device.api.ui.component.Dialog;
+import net.rim.device.api.ui.component.LabelField;
+import net.rim.device.api.ui.component.NumericChoiceField;
+import net.rim.device.api.ui.component.ObjectChoiceField;
 import net.rim.device.api.ui.component.SeparatorField;
 import net.rim.device.api.ui.container.MainScreen;
 import core.Proceso;
@@ -11,14 +15,24 @@ public class VerProceso extends MainScreen {
 	/**
 	 * 
 	 */
+	BasicEditField _txtDemandante;
+	BasicEditField _txtDemandado;
+	DateField _dfFecha;
+	BasicEditField _txtJuzgado;
+	BasicEditField _radicado;
+	BasicEditField _radicadoUnico;
+	ObjectChoiceField _actuaciones;
+	BasicEditField _estado;
+	BasicEditField _categoria;
+	BasicEditField _tipo;
+	BasicEditField _notas;
+	NumericChoiceField _prioridad;
+	
+	
 	public VerProceso(Object proceso) {
 		super(MainScreen.VERTICAL_SCROLL | MainScreen.VERTICAL_SCROLLBAR);
 		// TODO Auto-generated constructor stub
 		setTitle("Proceso");
-
-		String string = getFont() + "" + getFont().getHeight() + "";
-
-		Dialog.alert(string);
 
 		BasicEditField txtDemandante = new BasicEditField();
 		txtDemandante.setLabel("Demandante: ");
