04f2dacd2c8929ec2451c969dec222415064eb43
Harold <sancospi@gmail.com>
2011-06-16 17:01:43 -0500

    Se deja un Main y ScreenMain standar y trabajar con clases de prueba
    Change-Id: I9fe1f5999f73304c18c92e661db5c2c38145c4a1

--- src/ehmsoft/Main.java
+++ src/ehmsoft/Main.java
@@ -1,38 +1,3 @@
-/*package ehmsoft;
-
-import net.rim.device.api.ui.UiApplication;
-
-*//**
- * This class extends the UiApplication class, providing a
- * graphical user interface.
- *//*
-public class Main extends UiApplication
-{
-    *//**
-     * Entry point for application
-     * @param args Command line arguments (not used)
-     *//*
-	Controller controller;
-    public static void main(String[] args)
-    {
-        // Create a new instance of the application and make the currently
-        // running thread the application's event dispatch thread.
-        Main theApp = new Main();       
-        theApp.enterEventDispatcher();
-    }
-    
-
-    *//**
-     * Creates a new Main object
-     *//*
-    public Main()
-    {        
-        // Push a screen onto the UI stack for rendering.
-    	controller = new Controller();
-        pushScreen(new screenMain());
-    }    
-}*/
-
 package ehmsoft;
 
 import net.rim.device.api.ui.UiApplication;
@@ -62,6 +27,6 @@ public class Main extends UiApplication
     public Main()
     {        
         // Push a screen onto the UI stack for rendering.
-        pushScreen(new ScreenMain(this));
+        pushScreen(new ScreenMain());
     }
 }
