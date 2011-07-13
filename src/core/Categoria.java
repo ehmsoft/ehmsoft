7769755f961f7e45cfdeac0627bb67dffe8c8a7d
elfotografo007 <elfotografo007@gmail.com>
2011-07-13 09:42:37 -0500

    Se crea la clase categoria en Core
    Change-Id: I646dd293642aaabe057b6e17d7430b9c5b451442

---
+++ src/core/Categoria.java
@@ -0,0 +1,26 @@
+package core;
+
+public class Categoria {
+	private String id_categoria;
+	private String descripcion;
+	public Categoria(String id_categoria, String descripcion) {
+		this.id_categoria = id_categoria;
+		this.descripcion = descripcion;
+	}
+	public Categoria(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public String getId_categoria() {
+		return id_categoria;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setId_categoria(String id_categoria) {
+		this.id_categoria = id_categoria;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	
+}
