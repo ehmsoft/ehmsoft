ecedd138990a199188e4436a8bad7d2339f40d58
Harold <sancospi@gmail.com>
2011-07-15 22:59:49 -0500

    Se implementa menú contextual

--- src/gui/ListadoActuacionesScreen.java
+++ src/gui/ListadoActuacionesScreen.java
@@ -5,6 +5,7 @@ import persistence.Persistence;
 import net.rim.device.api.ui.MenuItem;
 import net.rim.device.api.ui.UiApplication;
 import net.rim.device.api.ui.component.Dialog;
+import net.rim.device.api.ui.component.Menu;
 import net.rim.device.api.ui.container.MainScreen;
 import core.Actuacion;
 import core.Proceso;
@@ -43,8 +44,13 @@ public class ListadoActuacionesScreen extends MainScreen {
 
 		_lista.insert(0, "Nueva actuación");
 		add(_lista);
-		addMenuItem(menuVer);
-		addMenuItem(menuDelete);
+	}
+	
+	protected void makeMenu(Menu menu, int instance) {
+		if (!String.class.isInstance(_lista.get(_lista, _lista.getSelectedIndex()))) {
+			menu.add(menuVer);
+			menu.add(menuDelete);
+		}
 	}
 
 	private final MenuItem menuVer = new MenuItem("Ver", 0, 0) {
