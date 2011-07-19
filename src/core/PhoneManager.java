5f81ce79c6f799db919311b93398030e07a3d3c1
elfotografo007 <elfotografo007@gmail.com>
2011-07-19 01:03:56 -0500

    Se implementa PhoneManager
    Change-Id: I9362d6dfded6fb921e4614429ebd01faa30b8bab

---
+++ src/core/PhoneManager.java
@@ -0,0 +1,10 @@
+package core;
+
+import net.rim.blackberry.api.invoke.Invoke;
+import net.rim.blackberry.api.invoke.PhoneArguments;
+
+public class PhoneManager {
+	public static void call(String number){
+		Invoke.invokeApplication(Invoke.APP_TYPE_PHONE,new PhoneArguments(PhoneArguments.ARG_CALL, number));
+	}
+}
