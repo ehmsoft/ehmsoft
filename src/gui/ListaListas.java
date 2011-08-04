7dcea4288ee0266a1dc9714a1f5a023bf20c0300
sancospi <sancospi@gmail.com>
2011-08-04 16:43:59 -0500

    Se modifica dibujado por defecto
    Change-Id: I0e85ec012e04ee83624249c132f35393d90259ab

--- src/gui/ListaListas.java
+++ src/gui/ListaListas.java
@@ -1,14 +1,21 @@
 package gui;
 
+import net.rim.device.api.collection.ReadableList;
 import net.rim.device.api.collection.util.UnsortedReadableList;
+import net.rim.device.api.ui.Color;
+import net.rim.device.api.ui.Font;
+import net.rim.device.api.ui.Graphics;
 import net.rim.device.api.ui.component.KeywordFilterField;
 import net.rim.device.api.ui.component.KeywordProvider;
+import net.rim.device.api.ui.component.ListField;
+import net.rim.device.api.ui.component.ListFieldCallback;
 
 abstract class ListaListas extends KeywordFilterField {
 	Unsorted _u;
 
 	public ListaListas() {
 		super();
+		setCallback(new Callback());
 		_u = new Unsorted();
 	}
 
@@ -72,4 +79,36 @@ class Unsorted extends UnsortedReadableList {
 	public void update(Object old, Object nw) {
 		doUpdate(old, nw);
 	}
+}
+
+class Callback implements ListFieldCallback {
+
+	public void drawListRow(ListField listField, Graphics graphics, int index,
+			int y, int width) {
+		ReadableList r = ((ListaListas)listField).getResultList();
+		if(String.class.isInstance(r.getAt(index))) {
+			graphics.setFont(graphics.getFont().derive(Font.BOLD));
+			int color = graphics.getColor();
+			graphics.setColor(Color.LIGHTGRAY);
+			graphics.drawLine(5, listField.getRowHeight() + y - 1, listField.getWidth() - 5, listField.getRowHeight() + y - 1);
+			graphics.setColor(color);
+		}
+		graphics.drawText(r.getAt(index).toString(), 0, y);
+	}
+
+	public Object get(ListField listField, int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int getPreferredWidth(ListField listField) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int indexOfList(ListField listField, String prefix, int start) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
 }
