7891ec88e7a9228a34f98f1f47595ff2bdae57a0
Harold <sancospi@gmail.com>
2011-08-10 23:06:38 -0500

    Se implementa clase abstracta para managers internos Se actualiza el manager de actuaciones acorde a estos cambios

--- src/gui/ActuacionesManager.java
+++ src/gui/ActuacionesManager.java
@@ -5,48 +5,32 @@ import java.util.Vector;
 import core.Actuacion;
 
 import persistence.Persistence;
-import net.rim.device.api.system.Display;
-import net.rim.device.api.ui.Field;
-import net.rim.device.api.ui.FocusChangeListener;
-import net.rim.device.api.ui.Font;
-import net.rim.device.api.ui.Graphics;
-import net.rim.device.api.ui.Manager;
-import net.rim.device.api.ui.XYEdges;
 import net.rim.device.api.ui.component.Dialog;
 import net.rim.device.api.ui.component.LabelField;
 import net.rim.device.api.ui.component.SeparatorField;
 import net.rim.device.api.ui.container.HorizontalFieldManager;
 import net.rim.device.api.ui.container.VerticalFieldManager;
-import net.rim.device.api.ui.decor.Border;
-import net.rim.device.api.ui.decor.BorderFactory;
 
-public class ActuacionesManager extends Manager {
+public class ActuacionesManager extends CustomManager {
 
-	private ListadoActuacionesLista _lista;
-	private ListadoManager _manager;
-	private VerticalFieldManager _title;
 	private LabelField _juzgado;
 	private LabelField _fecha;
 	private LabelField _fechaProxima;
 	private LabelField _descripcion;
-	private VerticalFieldManager _vertical;
 
 	public ActuacionesManager(int cant) {
-		super(0);
+		super();
+		setTitle("Actuaciones críticas");
 		Persistence p;
-		Font t = getFont();
-		int s = t.getStyle();
-		int h = t.getHeight();
-		int r = (int) (Display.getHeight() * 0.02777);
-		t = t.derive(s, h - r);
-		setFont(t);
-		Font bold = getFont().derive(Font.BOLD);
-		_vertical = new VerticalFieldManager(Field.NON_FOCUSABLE);
 		_lista = new ListadoActuacionesLista();
-		_title = new VerticalFieldManager(VerticalFieldManager.USE_ALL_WIDTH);
-		_title.add(new LabelField("Actuaciones críticas", Field.FIELD_HCENTER));
-		_title.add(new SeparatorField());
-		_title.setFont(bold);
+		try {
+			p = new Persistence();
+			Vector v = p.consultarActuacionesCriticas(cant);
+			_lista.loadFrom(v);
+		} catch (Exception e) {
+			Dialog.alert(e.toString());
+		}
+		setLista(_lista);
 
 		HorizontalFieldManager juzgado = new HorizontalFieldManager();
 		HorizontalFieldManager fecha = new HorizontalFieldManager();
@@ -55,13 +39,13 @@ public class ActuacionesManager extends Manager {
 		HorizontalFieldManager descripcion = new HorizontalFieldManager();
 
 		descripcion.add(new LabelField("Descripción: "));
-		descripcion.getField(0).setFont(bold);
+		descripcion.getField(0).setFont(_bold);
 		juzgado.add(new LabelField("Juzgado: "));
-		juzgado.getField(0).setFont(bold);
+		juzgado.getField(0).setFont(_bold);
 		fecha.add(new LabelField("Fecha: "));
-		fecha.getField(0).setFont(bold);
+		fecha.getField(0).setFont(_bold);
 		fechas.add(new LabelField("Fecha"));
-		fechas.setFont(bold);
+		fechas.setFont(_bold);
 		fechas.add(new LabelField("próxima: "));
 		fechaP.add(fechas);
 
@@ -77,26 +61,7 @@ public class ActuacionesManager extends Manager {
 		fecha.add(_fecha);
 		fechaP.add(_fechaProxima);
 		descripcion.add(_descripcion);
-
-		try {
-			p = new Persistence();
-			Vector v = p.consultarActuacionesCriticas(cant);
-			_lista.loadFrom(v);
-		} catch (Exception e) {
-			Dialog.alert(e.toString());
-		}
-
-		t = _lista.getFont();
-		s = t.getStyle();
-		h = t.getHeight();
-		r = (int) (Display.getHeight() * 0.02777);
-		t = t.derive(s, h - r);
-
-		Border border = BorderFactory.createRoundedBorder(new XYEdges(r, r, r,
-				r));
-
-		_lista.setFont(t);
-		_lista.setRowHeight(_lista.getFont().getHeight() * 2);
+		
 		_vertical.add(descripcion);
 		_vertical.add(new SeparatorField());
 		_vertical.add(juzgado);
@@ -105,21 +70,12 @@ public class ActuacionesManager extends Manager {
 		_vertical.add(new SeparatorField());
 		_vertical.add(fechaP);
 
-		_manager = new ListadoManager(_lista);
-		_manager.setBorder(border);
 		_lista.setFocusListener(listenerLista);
 		_lista.setSelectedIndex(0);
 		add(_manager);
 		add(_title);
 		add(_vertical);
 	}
-
-	FocusChangeListener listenerLista = new FocusChangeListener() {
-
-		public void focusChanged(Field field, int eventType) {
-			rightUpdate();
-		}
-	};
 
 	void rightUpdate() {
 		try {
@@ -140,53 +96,5 @@ public class ActuacionesManager extends Manager {
 			_vertical.invalidate();
 		} catch (NullPointerException e) {
 		}
-	}
-
-	protected void sublayout(int width, int height) {
-		int p = (int) (Display.getHeight() * 0.02777);
-		int w = (int) (Display.getWidth() * 0.0333333333333);
-		int h = (int) (Display.getHeight() * 0.133333333);
-
-		layoutChild(_manager, (width / 2), Display.getHeight() - h - (w * 2));
-		layoutChild(_title, (width / 2) - (p * 2), getFont().getHeight()
-				+ (p / 2));
-		layoutChild(_vertical, (width / 2) - (p * 2), height
-				- (getFont().getHeight() * 2));
-
-		setPositionChild(_manager, 0, 0);
-		setPositionChild(_title, (width / 2) + p, 0);
-		setPositionChild(_vertical, (width / 2) + p, getFont().getHeight() * 2
-				+ ((p / 2) + p));
-
-		setExtent(Display.getWidth() - (w * 2), Display.getHeight() - h
-				- (w * 2));
-	}
-}
-
-class ListadoManager extends Manager {
-
-	ListaListas _lista;
-
-	public ListadoManager(ListaListas l) {
-		super(Manager.VERTICAL_SCROLL);
-		_lista = l;
-		add(_lista);
-		_lista.setSelectedIndex(0);
-	}
-
-	protected void sublayout(int width, int height) {
-		int p = (int) (Display.getHeight() * 0.02777);
-		int w = (int) (Display.getWidth() * 0.0333333333333);
-		int h = (int) (Display.getHeight() * 0.133333333);
-
-		layoutChild(_lista, width + p, Display.getHeight() - h - (w * 2) - p);
-		setPositionChild(_lista, 0, 0);
-		setExtent((Display.getWidth() - (w * 2)) / 2 - (p * 3),
-				Display.getHeight() - h - (w * 2) - 2 * p);
-	}
-
-	protected void paint(Graphics g) {
-		g.setDrawingStyle(Graphics.DRAWSTYLE_AAPOLYGONS, true);
-		super.paint(g);
 	}
 }
---
+++ src/gui/CustomManager.java
@@ -0,0 +1,119 @@
+package gui;
+
+import net.rim.device.api.system.Display;
+import net.rim.device.api.ui.Field;
+import net.rim.device.api.ui.FocusChangeListener;
+import net.rim.device.api.ui.Font;
+import net.rim.device.api.ui.Graphics;
+import net.rim.device.api.ui.Manager;
+import net.rim.device.api.ui.XYEdges;
+import net.rim.device.api.ui.component.LabelField;
+import net.rim.device.api.ui.component.SeparatorField;
+import net.rim.device.api.ui.container.VerticalFieldManager;
+import net.rim.device.api.ui.decor.Border;
+import net.rim.device.api.ui.decor.BorderFactory;
+
+public abstract class CustomManager extends Manager{
+	
+	protected ListaListas _lista;
+	protected ListadoManager _manager;
+	protected VerticalFieldManager _title;
+	protected VerticalFieldManager _vertical;
+	protected Font _bold;
+	
+	public CustomManager() {
+		super(0);
+		Font t = getFont();
+		int s = t.getStyle();
+		int h = t.getHeight();
+		int r = (int) (Display.getHeight() * 0.02777);
+		t = t.derive(s, h - r);
+		setFont(t);
+		_bold = getFont().derive(Font.BOLD);
+		_vertical = new VerticalFieldManager(Field.NON_FOCUSABLE);
+		_lista = new ListadoActuacionesLista();
+		_title = new VerticalFieldManager(VerticalFieldManager.USE_ALL_WIDTH);
+		_title.add(new LabelField("Manager generico", Field.FIELD_HCENTER));
+		_title.add(new SeparatorField());
+		_title.setFont(_bold);
+	}
+	
+	FocusChangeListener listenerLista = new FocusChangeListener() {
+
+		public void focusChanged(Field field, int eventType) {
+			rightUpdate();
+		}
+	};
+
+	void rightUpdate() {
+	}
+	
+	protected void setTitle(String title) {
+		((LabelField)_title.getField(0)).setText(title);
+	}
+	
+	protected void setLista(ListaListas lista) {
+		_lista = lista;
+		Font t = _lista.getFont();
+		int s = t.getStyle();
+		int h = t.getHeight();
+		int r = (int) (Display.getHeight() * 0.02777);
+		t = t.derive(s, h - r);
+
+		Border border = BorderFactory.createRoundedBorder(new XYEdges(r, r, r,
+				r));
+
+		_lista.setFont(t);
+		_lista.setRowHeight(_lista.getFont().getHeight() * 2);
+		_manager = new ListadoManager(_lista);
+		_manager.setBorder(border);
+	}
+	
+	protected void sublayout(int width, int height) {
+		int p = (int) (Display.getHeight() * 0.02777);
+		int w = (int) (Display.getWidth() * 0.0333333333333);
+		int h = (int) (Display.getHeight() * 0.133333333);
+
+		layoutChild(_manager, (width / 2), Display.getHeight() - h - (w * 2));
+		layoutChild(_title, (width / 2) - (p * 2), getFont().getHeight()
+				+ (p / 2));
+		layoutChild(_vertical, (width / 2) - (p * 2), height
+				- (getFont().getHeight() * 2));
+
+		setPositionChild(_manager, 0, 0);
+		setPositionChild(_title, (width / 2) + p, 0);
+		setPositionChild(_vertical, (width / 2) + p, getFont().getHeight() * 2
+				+ ((p / 2) + p));
+
+		setExtent(Display.getWidth() - (w * 2), Display.getHeight() - h
+				- (w * 2));
+	}
+}
+
+class ListadoManager extends Manager {
+
+	ListaListas _lista;
+
+	public ListadoManager(ListaListas l) {
+		super(Manager.VERTICAL_SCROLL);
+		_lista = l;
+		add(_lista);
+		_lista.setSelectedIndex(0);
+	}
+
+	protected void sublayout(int width, int height) {
+		int p = (int) (Display.getHeight() * 0.02777);
+		int w = (int) (Display.getWidth() * 0.0333333333333);
+		int h = (int) (Display.getHeight() * 0.133333333);
+
+		layoutChild(_lista, width + p, Display.getHeight() - h - (w * 2) - p);
+		setPositionChild(_lista, 0, 0);
+		setExtent((Display.getWidth() - (w * 2)) / 2 - (p * 3),
+				Display.getHeight() - h - (w * 2) - 2 * p);
+	}
+
+	protected void paint(Graphics g) {
+		g.setDrawingStyle(Graphics.DRAWSTYLE_AAPOLYGONS, true);
+		super.paint(g);
+	}
+}
