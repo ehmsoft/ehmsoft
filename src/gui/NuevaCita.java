278920616a35aee16336706cdd4202b6c46ef3a2
sancospi <sancospi@gmail.com>
2011-08-22 15:35:48 -0500

    Se implementa con respecto a un MVC más estricto

--- src/gui/NuevaCita.java
+++ src/gui/NuevaCita.java
@@ -2,65 +2,76 @@ package gui;
 
 import java.util.Date;
 
-import net.rim.device.api.ui.component.Dialog;
-
+import net.rim.device.api.ui.Field;
+import net.rim.device.api.ui.FieldChangeListener;
+import net.rim.device.api.ui.UiApplication;
 import core.CalendarManager;
 
 public class NuevaCita {
-	
+
 	private NuevaCitaPopUp _screen;
 	private String _uid;
-	
+
 	public NuevaCita(String descripcion, Date fecha) {
 		_screen = new NuevaCitaPopUp(descripcion, fecha);
+		_screen.setChangeListener(listener);
 	}
-	
+
+	FieldChangeListener listener = new FieldChangeListener() {
+
+		public void fieldChanged(Field field, int context) {
+			if (context == _screen.GUARDAR) {
+				guardarCita();
+			} else if (context == _screen.CERRAR) {
+				cerrarPantalla();
+			}
+		}
+	};
+
 	public NuevaCitaPopUp getScreen() {
 		return _screen;
 	}
-	
-	public boolean isGuardado() {
-		return _screen.isGuardado();
-	}
-	
-	public boolean isAlarma() {
-		return _screen.isAlarma();
-	}
-	
+
 	public String getUid() {
-		if(_uid == null) {
-			guardarCita();
-		}
 		return _uid;
 	}
-	
+
+	public boolean hasAlarma() {
+		return _screen.hasAlarma();
+	}
+
 	public void guardarCita() {
-		if(_screen.isGuardado()) {
-			if(_screen.isAlarma()) {
-				int duracion = _screen.getDuracion();
-				String intervalo = _screen.getIntervalo();
-				if(intervalo.equals("Días")) {
-					duracion = duracion * 86400;
-				}
-				else if(intervalo.equals("Horas")) {
-					duracion = duracion * 3600;
-				}
-				else if(intervalo.equals("Minutos")) {
-					duracion = duracion * 60;
-				}
-				
-				try {
-					_uid = CalendarManager.agregarCita(_screen.getFecha(), _screen.getDescripcion(), duracion);
-				} catch (Exception e) {
-					Dialog.alert(e.toString());
-				}
-			} else {
-				try {
-					_uid = CalendarManager.agregarCita(_screen.getFecha(), _screen.getDescripcion());
-				} catch (Exception e) {
-					Dialog.alert(e.toString());
-				}
+		if (_screen.hasAlarma()) {
+			int alarma = _screen.getDuracion();
+			String intervalo = _screen.getIntervalo();
+			if (intervalo.equalsIgnoreCase("dias")) {
+				alarma *= 86400;
+			} else if (intervalo.equalsIgnoreCase("horas")) {
+				alarma *= 3600;
+			} else if (intervalo.equalsIgnoreCase("minutos")) {
+				alarma *= 60;
+			}
+			try {
+				_uid = CalendarManager.agregarCita(_screen.getFecha(),
+						_screen.getDescripcion(), alarma);
+				UiApplication.getUiApplication().popScreen(_screen);
+			} catch (Exception e) {
+				_screen.setAlarma(false);
+				_screen.alert("No se pudo guardar la cita en el calendario");
+			}
+		} else {
+			try {
+				_uid = CalendarManager.agregarCita(_screen.getFecha(),
+						_screen.getDescripcion());
+				UiApplication.getUiApplication().popScreen(_screen);
+			} catch (Exception e) {
+				_screen.alert("No se pudo guardar la cita en el calendario");
+
 			}
 		}
+	}
+
+	private void cerrarPantalla() {
+		UiApplication.getUiApplication().popScreen(_screen);
 	}
 }
--- src/gui/NuevaCitaPopUp.java
+++ src/gui/NuevaCitaPopUp.java
@@ -4,10 +4,11 @@ import java.util.Date;
 
 import net.rim.device.api.ui.Field;
 import net.rim.device.api.ui.FieldChangeListener;
-import net.rim.device.api.ui.UiApplication;
 import net.rim.device.api.ui.component.BasicEditField;
 import net.rim.device.api.ui.component.ButtonField;
 import net.rim.device.api.ui.component.CheckboxField;
+import net.rim.device.api.ui.component.DateField;
+import net.rim.device.api.ui.component.Dialog;
 import net.rim.device.api.ui.component.LabelField;
 import net.rim.device.api.ui.component.ObjectChoiceField;
 import net.rim.device.api.ui.component.SeparatorField;
@@ -20,36 +21,38 @@ public class NuevaCitaPopUp extends PopupScreen {
 	private BasicEditField _txtDescripcion;
 	private CheckboxField _cbAlarma;
 	private BasicEditField _txtTiempo;
-	private ObjectChoiceField _nfTiempo;
-	private GridFieldManager _g;
-	private SeparatorField _s;
+	private ObjectChoiceField _ocTiempo;
+	private GridFieldManager _grid;
+	private SeparatorField _separator;
 	private ButtonField _btnAceptar;
 	private ButtonField _btnCancelar;
 	private LabelField _lblFecha;
+	private DateField _dfFecha;
 
-	private Date _fecha;
-
-	private boolean _guardar = false;
+	public final int GUARDAR = 1;
+	public final int CERRAR = 2;
 
 	public NuevaCitaPopUp(String descripcion, Date fecha) {
 		super(new VerticalFieldManager());
-
-		_fecha = fecha;
 
 		LabelField labelField = new LabelField("Crear cita",
 				Field.FIELD_HCENTER);
 		add(labelField);
 		add(new SeparatorField());
 
-		_g = new GridFieldManager(1, 2, 8);
-		_g.setColumnProperty(0, GridFieldManager.FIXED_SIZE, 200);
-		_g.setColumnProperty(1, GridFieldManager.PREFERRED_SIZE, 20);
+		_grid = new GridFieldManager(1, 2, 8);
+		_grid.setColumnProperty(0, GridFieldManager.FIXED_SIZE, 200);
+		_grid.setColumnProperty(1, GridFieldManager.PREFERRED_SIZE, 20);
 
 		_txtDescripcion = new BasicEditField("Descripción: ", descripcion);
 		add(_txtDescripcion);
 
 		_lblFecha = new LabelField(fecha.toString());
 		add(_lblFecha);
+
+		_dfFecha = new DateField("Fecha: ", fecha.getTime(),
+				DateField.DATE_TIME);
+		add(_dfFecha);
 
 		_cbAlarma = new CheckboxField("Alarma", false);
 		_cbAlarma.setChangeListener(listenerTiempo);
@@ -60,19 +63,19 @@ public class NuevaCitaPopUp extends PopupScreen {
 		_txtTiempo.setText("5");
 
 		Object[] choices = { "Minutos", "Horas", "Días" };
-		_nfTiempo = new ObjectChoiceField(null, choices, 0, FIELD_LEFT);
+		_ocTiempo = new ObjectChoiceField(null, choices, 0, FIELD_LEFT);
 
-		_g.add(_txtTiempo);
-		_g.add(_nfTiempo);
+		_grid.add(_txtTiempo);
+		_grid.add(_ocTiempo);
 
-		_s = new SeparatorField();
-		add(_s);
+		_separator = new SeparatorField();
+		add(_separator);
 		_btnAceptar = new ButtonField("Aceptar", ButtonField.CONSUME_CLICK
 				| Field.FIELD_HCENTER);
 		_btnAceptar.setChangeListener(listenerAceptar);
 		add(_btnAceptar);
-		_btnCancelar = new ButtonField("Cancelar",
-				ButtonField.CONSUME_CLICK | Field.FIELD_HCENTER);
+		_btnCancelar = new ButtonField("Cancelar", ButtonField.CONSUME_CLICK
+				| Field.FIELD_HCENTER);
 		_btnCancelar.setChangeListener(listenerCancelar);
 		add(_btnCancelar);
 	}
@@ -80,15 +83,14 @@ public class NuevaCitaPopUp extends PopupScreen {
 	private FieldChangeListener listenerAceptar = new FieldChangeListener() {
 
 		public void fieldChanged(Field field, int context) {
-			_guardar = true;
-			UiApplication.getUiApplication().popScreen(getScreen());
+			fieldChangeNotify(GUARDAR);
 		}
 	};
 
 	private FieldChangeListener listenerCancelar = new FieldChangeListener() {
 
 		public void fieldChanged(Field field, int context) {
-			UiApplication.getUiApplication().popScreen(getScreen());
+			fieldChangeNotify(CERRAR);
 		}
 	};
 
@@ -96,33 +98,37 @@ public class NuevaCitaPopUp extends PopupScreen {
 
 		public void fieldChanged(Field field, int context) {
 			if (_cbAlarma.getChecked()) {
-				delete(_s);
+				delete(_separator);
 				delete(_btnAceptar);
 				delete(_btnCancelar);
-				add(_g);
-				add(_s);
+				add(_grid);
+				add(_separator);
 				add(_btnAceptar);
 				add(_btnCancelar);
 			} else {
-				delete(_g);
+				delete(_grid);
 			}
 		}
 	};
+
+	public void setAlarma(boolean alarma) {
+		_cbAlarma.setChecked(alarma);
+	}
+
+	public void alert(String string) {
+		Dialog.alert(string);
+	}
 
 	public String getDescripcion() {
 		return _txtDescripcion.getText();
 	}
 
 	public Date getFecha() {
-		return _fecha;
+		return new Date(_dfFecha.getDate());
 	}
 
-	public boolean isAlarma() {
+	public boolean hasAlarma() {
 		return _cbAlarma.getChecked();
-	}
-
-	public boolean isGuardado() {
-		return _guardar;
 	}
 
 	public int getDuracion() {
@@ -130,6 +136,6 @@ public class NuevaCitaPopUp extends PopupScreen {
 	}
 
 	public String getIntervalo() {
-		return (String) _nfTiempo.getChoice(_nfTiempo.getSelectedIndex());
+		return (String) _ocTiempo.getChoice(_ocTiempo.getSelectedIndex());
 	}
 }
