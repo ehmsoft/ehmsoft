cd8975408c2e9b4ae17cf2266b6bfe4851309938
sancospi <sancospi@gmail.com>
2011-08-30 15:24:02 -0500

    Corrección no funcional a algunos fragmentos de código Se agregan setters y getters

--- src/gui/PreferenciasGeneralesScreen.java
+++ src/gui/PreferenciasGeneralesScreen.java
@@ -1,16 +1,17 @@
 package gui;
 
-import core.Preferencias;
 import net.rim.device.api.ui.Field;
 import net.rim.device.api.ui.FieldChangeListener;
 import net.rim.device.api.ui.Font;
 import net.rim.device.api.ui.FontFamily;
+import net.rim.device.api.ui.MenuItem;
 import net.rim.device.api.ui.XYEdges;
 import net.rim.device.api.ui.component.BasicEditField;
 import net.rim.device.api.ui.component.ButtonField;
 import net.rim.device.api.ui.component.CheckboxField;
 import net.rim.device.api.ui.component.Dialog;
 import net.rim.device.api.ui.component.LabelField;
+import net.rim.device.api.ui.component.Menu;
 import net.rim.device.api.ui.component.ObjectChoiceField;
 import net.rim.device.api.ui.component.RichTextField;
 import net.rim.device.api.ui.component.SeparatorField;
@@ -40,21 +41,21 @@ public class PreferenciasGeneralesScreen extends MainScreen {
 	String [] _listaEstilos = {"Normal", "Negrita", "Extra negrita", "Negrita cursiva", "Cursiva"};
 	public PreferenciasGeneralesScreen() {
 		super(MainScreen.VERTICAL_SCROLL | MainScreen.VERTICAL_SCROLLBAR);
-		_txtNombreUsuario = new BasicEditField("Nombre de Usuario: ", Preferencias.getNombreUsuario());
-		_chkMostrarCampoBusqueda = new CheckboxField("Mostrar Búsqueda en todas las pantallas", Preferencias.isMostrarCampoBusqueda());
-		_chkMostrarTitulosPantalla = new CheckboxField("Mostrar los títulos de cada pantalla", Preferencias.isMostrarTitulosPantallas());
+		_txtNombreUsuario = new BasicEditField("Nombre de Usuario: ", "");
+		_chkMostrarCampoBusqueda = new CheckboxField("Mostrar Búsqueda en todas las pantallas", false);
+		_chkMostrarTitulosPantalla = new CheckboxField("Mostrar los títulos de cada pantalla", false);
 		//TODO inicializar el campo chfPantallaInicial, de él depende txtCantidadActuaciones
-		_chkRecordarUltimaCategoria = new CheckboxField("Recordar última categoría", Preferencias.isRecodarUltimaCategoria());
+		_chkRecordarUltimaCategoria = new CheckboxField("Recordar última categoría", false);
 		
 		//Fuentes
 		FontFamily[] listaFuentes = FontFamily.getFontFamilies();
 		_chfTipoFuente = new ObjectChoiceField("Tipo de fuente: ", listaFuentes, getFont().getFontFamily());
 		int[] _listaTamanos = listaFuentes[_chfTipoFuente.getSelectedIndex()].getHeights();
-		String[] _listaTamanosString = new String[_listaTamanos.length];
+		Integer[] _listaTamanosInteger = new Integer[_listaTamanos.length];
 		for(int i=0;i<_listaTamanos.length;i++){
-			_listaTamanosString[i] = Integer.toString(_listaTamanos[i]);
+			_listaTamanosInteger[i] = new Integer(_listaTamanos[i]);
 		}
-		_chfTamanoFuente = new ObjectChoiceField("Tamaño fuente: ", _listaTamanosString, Integer.toString(getFont().getHeight()));
+		_chfTamanoFuente = new ObjectChoiceField("Tamaño fuente: ", _listaTamanosInteger, new Integer(getFont().getHeight()));
 		_chfEstiloFuente = new ObjectChoiceField("Estilo de fuente:", _listaEstilos);
 		_txtEjemploFuente = new RichTextField("El veloz murciélago hindú comía feliz cardillo y kiwi. La cigüeña tocaba el saxofón detrás del palenque de paja.", Field.NON_FOCUSABLE | Field.USE_ALL_WIDTH);
 		_txtEjemploFuente.setFont(listaFuentes[_chfTipoFuente.getSelectedIndex()].getFont(_significadoEstilos[_chfEstiloFuente.getSelectedIndex()], _listaTamanos[_chfTamanoFuente.getSelectedIndex()]));
@@ -89,6 +90,53 @@ public class PreferenciasGeneralesScreen extends MainScreen {
 		add(_btnRestaurarPrefs);
 	
 	}
+	
+	public void setNombreUsuario(String text) {
+		_txtNombreUsuario.setText(text);
+	}
+	
+	public void setMostrarBusqueda(boolean value) {
+		_chkMostrarCampoBusqueda.setChecked(value);
+	}
+	
+	public void setMostrarTitulos(boolean value) {
+		_chkMostrarTitulosPantalla.setChecked(value);
+	}
+	
+	public void setRecordarUltimaCategoria(boolean value) {
+		_chkRecordarUltimaCategoria.setChecked(value);
+	}
+	
+	public void setFuente(Font font) {
+		_chfTipoFuente.setSelectedIndex(font.getFontFamily());
+		_chfEstiloFuente.setSelectedIndex(_significadoEstilos[font.getStyle()]);
+		_chfTamanoFuente.setSelectedIndex(new Integer(font.getHeight()));
+	}
+	
+	public String getNombreUsuario() {
+		return _txtNombreUsuario.getText();
+	}
+	
+	public boolean isMostrarBusqueda() {
+		return _chkMostrarCampoBusqueda.getChecked();
+	}
+	
+	public boolean isMostrarTitulos() {
+		return _chkMostrarTitulosPantalla.getChecked();
+	}
+	
+	public boolean isRecordarUltimaCategoria() {
+		return _chkRecordarUltimaCategoria.getChecked();
+	}
+	
+	public Font getFuente() {
+		FontFamily fontFamily = (FontFamily) _chfTipoFuente.getChoice(_chfTipoFuente.getSelectedIndex());
+		int style = _significadoEstilos[_chfEstiloFuente.getSelectedIndex()];
+		int height = ((Integer) _chfTamanoFuente.getChoice(_chfTamanoFuente.getSelectedIndex())).intValue();
+		Font font = fontFamily.getFont(style, height);
+		return font;
+	}
+	
 	private FieldChangeListener listenerPantallaInicial = new FieldChangeListener() {
 		
 		public void fieldChanged(Field field, int context) {
@@ -99,29 +147,34 @@ public class PreferenciasGeneralesScreen extends MainScreen {
 	private FieldChangeListener listenerTipoFuente = new FieldChangeListener() {
 		
 		public void fieldChanged(Field field, int context) {
-			FontFamily font = (FontFamily) _chfTipoFuente.getChoice(_chfTipoFuente.getSelectedIndex());
-			int[] _listaTamanos = font.getHeights();
-			String[] _listaTamanosString = new String[_listaTamanos.length];
+			FontFamily fontFamily = (FontFamily) _chfTipoFuente.getChoice(_chfTipoFuente.getSelectedIndex());
+			int[] _listaTamanos = fontFamily.getHeights();
+			Integer[] _listaTamanosInteger = new Integer[_listaTamanos.length];
 			for(int i=0;i<_listaTamanos.length;i++){
-				_listaTamanosString[i] = Integer.toString(_listaTamanos[i]);
+				_listaTamanosInteger[i] = new Integer(_listaTamanos[i]);
 			}
-			_chfTamanoFuente.setChoices(_listaTamanosString);
-			_chfTamanoFuente.setSelectedIndex(Integer.toString(getFont().getHeight()));
-			_txtEjemploFuente.setFont(font.getFont(_significadoEstilos[_chfEstiloFuente.getSelectedIndex()], Integer.parseInt((String) _chfTamanoFuente.getChoice(_chfTamanoFuente.getSelectedIndex()))));
+			int style = _significadoEstilos[_chfEstiloFuente.getSelectedIndex()];
+			int height = ((Integer) _chfTamanoFuente.getChoice(_chfTamanoFuente.getSelectedIndex())).intValue();
+			Font font = fontFamily.getFont(style, height);
+			_chfTamanoFuente.setChoices(_listaTamanosInteger);
+			_chfTamanoFuente.setSelectedIndex(new Integer(getFont().getHeight()));
+			_txtEjemploFuente.setFont(font);
 		}
 	};
 	private FieldChangeListener listenerActualizarFuente = new FieldChangeListener() {
 		
 		public void fieldChanged(Field field, int context) {
-			FontFamily font = (FontFamily) _chfTipoFuente.getChoice(_chfTipoFuente.getSelectedIndex());
-			_txtEjemploFuente.setFont(font.getFont(_significadoEstilos[_chfEstiloFuente.getSelectedIndex()], Integer.parseInt((String) _chfTamanoFuente.getChoice(_chfTamanoFuente.getSelectedIndex()))));
+			FontFamily fontFamily = (FontFamily) _chfTipoFuente.getChoice(_chfTipoFuente.getSelectedIndex());
+			int style = _significadoEstilos[_chfEstiloFuente.getSelectedIndex()];
+			int height = ((Integer) _chfTamanoFuente.getChoice(_chfTamanoFuente.getSelectedIndex())).intValue();
+			Font font = fontFamily.getFont(style, height);
+			_txtEjemploFuente.setFont(font);
 		}
 	};
 	private FieldChangeListener listenerCopiaSeguridad = new FieldChangeListener() {
 		
 		public void fieldChanged(Field field, int context) {
-			// TODO Listener Copia Seguridad
-			
+			fieldChangeNotify(Util.COPIA_SEGURIDAD);
 		}
 	};
 	private FieldChangeListener listenerRestaurarPrefs = new FieldChangeListener() {
@@ -132,11 +185,23 @@ public class PreferenciasGeneralesScreen extends MainScreen {
 			if(eleccion == 0){
 				// TODO Listener Preferencias por defecto
 			}
+			fieldChangeNotify(Util.RESTAURAR_PREFERENCIAS);
 		}
 	};
 	
+	protected void makeMenu(Menu menu, int instance) {
+		menu.add(menuGuardar);
+	}
+	
+	private MenuItem menuGuardar = new MenuItem("Guardar", 0, 0) {
+		
+		public void run() {
+			fieldChangeNotify(Util.GUARDAR);
+		}
+	};
+
 	protected boolean onSave(){
-		//TODO método al guardar
-		return true;
+		fieldChangeNotify(Util.GUARDAR);
+		return false;
 	}
 }
