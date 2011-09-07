27fcd6808058d59064b1af2b015aa99dbdc4db95
elfotografo007 <elfotografo007@gmail.com>
2011-09-07 11:32:25 -0500

    Se remueve un import no usado
    Change-Id: Icb94fd7a86802ee3623b257296943b43a0deb107

--- src/gui/About.java
+++ src/gui/About.java
@@ -7,7 +7,6 @@ import net.rim.device.api.ui.UiApplication;
 import net.rim.device.api.ui.component.BitmapField;
 import net.rim.device.api.ui.component.LabelField;
 import net.rim.device.api.ui.container.FullScreen;
-import net.rim.device.api.ui.container.MainScreen;
 import net.rim.device.api.ui.decor.BackgroundFactory;
 
 public class About extends FullScreen {
