0045daf1568f103e0e320bdfba0d3421e81ccd9b
elfotografo007 <elfotografo007@gmail.com>
2011-09-23 15:49:20 -0500

    Se crea clase ActuacionCritica
    Change-Id: I6a8423a52943a6b1f20fe5625011d324480f5f5b

---
+++ src/core/ActuacionCritica.java
@@ -0,0 +1,43 @@
+package core;
+
+import java.util.Calendar;
+
+public class ActuacionCritica extends Actuacion {
+	private String id_proceso;
+
+	public ActuacionCritica(Juzgado juzgado, Calendar fecha,
+			Calendar fechaProxima, String descripcion, String id_proceso) {
+		super(juzgado, fecha, fechaProxima, descripcion);
+		this.id_proceso = id_proceso;
+	}
+
+	public ActuacionCritica(Juzgado juzgado, Calendar fecha,
+			Calendar fechaProxima, String descripcion, String id_actuacion,
+			String id_proceso) {
+		super(juzgado, fecha, fechaProxima, descripcion, id_actuacion);
+		this.id_proceso = id_proceso;
+	}
+
+	public ActuacionCritica(Juzgado juzgado, Calendar fecha,
+			Calendar fechaProxima, String descripcion, String id_actuacion,
+			String uid, String id_proceso) {
+		super(juzgado, fecha, fechaProxima, descripcion, id_actuacion, uid);
+		this.id_proceso = id_proceso;
+	}
+
+	public ActuacionCritica(Actuacion actuacion, String id_proceso) {
+		super(actuacion.getJuzgado(), actuacion.getFecha(), actuacion
+				.getFechaProxima(), actuacion.getDescripcion(), actuacion
+				.getId_actuacion(), actuacion.getUid());
+		this.id_proceso = id_proceso;
+	}
+
+	public String getId_proceso() {
+		return id_proceso;
+	}
+
+	public void setId_proceso(String id_proceso) {
+		this.id_proceso = id_proceso;
+	}
+
+}
