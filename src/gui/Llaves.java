cfc77a2f0b55f43da0ca6313623198279c271b18
sancospi <sancospi@gmail.com>
2011-10-06 14:44:01 -0500

    Se restaura versión funcional del archivo

--- src/gui/Llaves.java
+++ src/gui/Llaves.java
@@ -3,7 +3,6 @@ package gui;
 import net.rim.device.api.system.DeviceInfo;
 import net.rim.device.api.ui.Field;
 import net.rim.device.api.ui.FieldChangeListener;
-import net.rim.device.api.ui.UiApplication;
 import persistence.Persistence;
 
 public class Llaves {
@@ -51,25 +50,17 @@ public class Llaves {
 	}
 
 	private void guardarLlave() {
-		Util.pushWaitScreen();
-		UiApplication.getUiApplication().invokeLater(new Runnable() {
-			
-			public void run() {
-				try {
-					_llave = _screen.getLlave();
-					if (verificarLlaves()) {
-						new Persistence().actualizarPreferencia(998, _llave);
-					} else {
-						_screen.alert("Lo sentimos, su llave no coincide. Verifique que la digitó correctamente. Si el problema persiste póngase en contacto con soporte técnico: soporte@ehmsoft.com");
-					}
-				} catch (NullPointerException npe) {
-					Util.noSd();
-				} catch (Exception e) {
-					Util.alert("No se ha podido verificar la llave de licencia. Error Desconocido");
-				} finally {
-					Util.popWaitScreen();
-				}
+		try {
+			_llave = _screen.getLlave();
+			if (verificarLlaves()) {
+				new Persistence().actualizarPreferencia(998, _llave);
+			} else {
+				_screen.alert("Lo sentimos, su llave no coincide. Verifique que la digitó correctamente. Si el problema persiste póngase en contacto con soporte técnico: soporte@ehmsoft.com");
 			}
-		});
+		} catch (NullPointerException npe) {
+			Util.noSd();
+		} catch (Exception e) {
+			Util.alert("No se ha podido verificar la llave de licencia. Error Desconocido");
+		}
 	}
 }
