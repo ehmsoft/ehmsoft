b5837e7df8dad2f1811bb1633847e10006bdc8f9
sancospi <sancospi@gmail.com>
2011-10-05 21:07:31 -0500

    Se agregan mensajes de espera en los accesos a la base de datos

--- src/gui/Llaves.java
+++ src/gui/Llaves.java
@@ -3,6 +3,7 @@ package gui;
 import net.rim.device.api.system.DeviceInfo;
 import net.rim.device.api.ui.Field;
 import net.rim.device.api.ui.FieldChangeListener;
+import net.rim.device.api.ui.UiApplication;
 import persistence.Persistence;
 
 public class Llaves {
@@ -12,15 +13,23 @@ public class Llaves {
 	public Llaves() {
 		_screen = new LlavesScreen();
 		_screen.setChangeListener(listener);
-		try {
-			_llave = new Persistence().consultarPreferencia(998);
-			_screen.setLlave(_llave);
-			verificarLlaves();
-		} catch (NullPointerException npe) {
-			Util.noSd();
-		} catch (Exception e) {
-			Util.alert("No se ha podido verificar la llave de licencia. Error Desconocido");
-		}
+		Util.pushWaitScreen();
+		UiApplication.getUiApplication().invokeLater(new Runnable() {
+			
+			public void run() {
+				try {
+					_llave = new Persistence().consultarPreferencia(998);
+					_screen.setLlave(_llave);
+					verificarLlaves();
+				} catch (NullPointerException npe) {
+					Util.noSd();
+				} catch (Exception e) {
+					Util.alert("No se ha podido verificar la llave de licencia. Error Desconocido");
+				} finally {
+					Util.popWaitScreen();
+				}
+			}
+		});
 	}
 
 	private FieldChangeListener listener = new FieldChangeListener() {
@@ -50,17 +59,25 @@ public class Llaves {
 	}
 
 	private void guardarLlave() {
-		try {
-			_llave = _screen.getLlave();
-			if (verificarLlaves()) {
-				new Persistence().actualizarPreferencia(998, _llave);
-			} else {
-				_screen.alert("Lo sentimos, su llave no coincide. Verifique que la digitó correctamente. Si el problema persiste póngase en contacto con soporte técnico: soporte@ehmsoft.com");
+		Util.pushWaitScreen();
+		UiApplication.getUiApplication().invokeLater(new Runnable() {
+			
+			public void run() {
+				try {
+					_llave = _screen.getLlave();
+					if (verificarLlaves()) {
+						new Persistence().actualizarPreferencia(998, _llave);
+					} else {
+						_screen.alert("Lo sentimos, su llave no coincide. Verifique que la digitó correctamente. Si el problema persiste póngase en contacto con soporte técnico: soporte@ehmsoft.com");
+					}
+				} catch (NullPointerException npe) {
+					Util.noSd();
+				} catch (Exception e) {
+					Util.alert("No se ha podido verificar la llave de licencia. Error Desconocido");
+				} finally {
+					Util.popWaitScreen();
+				}
 			}
-		} catch (NullPointerException npe) {
-			Util.noSd();
-		} catch (Exception e) {
-			Util.alert("No se ha podido verificar la llave de licencia. Error Desconocido");
-		}
+		});
 	}
 }
